24ebd10b7ad62164b76142b71642967f56779e1f
Nikhil Vijayan <nkhilv@gmail.com>
2018-12-11 19:39:22 +0000

    Add new methods ie refactored code

--- directory.rb
+++ directory.rb
@@ -1,20 +1,23 @@
 #!/usr/bin/env ruby
 
 
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students 
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
 
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do
+    students << { name: name, cohort: :November }
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+
+  students
+
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -23,15 +26,15 @@ end
 
 def print(students)
   students.each do |student|
-    puts "#{ student[:name]} #{student[:cohort]} cohort "
-
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
