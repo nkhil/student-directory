54948dc9d9260547b19dceebcbc81b8937919c77
Nikhil Vijayan <nkhilv@gmail.com>
2018-12-11 22:52:10 +0000

    Add load students method that reads the file that save_students creates

--- directory.rb
+++ directory.rb
@@ -71,10 +71,19 @@ def save_students
   # open the file for writing
   file = File.open("students.csv", "w")
   # iterate over the array of students
-  @students.each do |student|
+  $students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    $students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
